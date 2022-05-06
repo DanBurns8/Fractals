c6e04bbf26370b568ca66d3974195fc0eb3aa92a
Daniel Burns <danielburns@billericak12.com>
2022-05-06 12:01:18 -0400

    Daniel Burns

--- src/SeripinskisTriangle.java
+++ src/SeripinskisTriangle.java
@@ -8,42 +8,45 @@ import javax.swing.JPanel;
  *  Square Rose, a recursive algorithm
  */
 
-public class SquareRose extends JPanel {
+public class SeripinskisTriangle extends JPanel
+{
     private int levels;
-
-    public SquareRose(int lev) {
+    public SeripinskisTriangle(int lev)
+    {
         levels = lev;
     }
-
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g)
+    {
         super.paintComponent(g);  // Call JPanel's paintComponent method
         //   to paint the background
         int xCenter = getWidth() / 2;
         int yCenter = getHeight() / 2;
 
 
-        int[] xcoord = {xCenter - 128, xCenter - 128, xCenter + 128, xCenter + 128};
-        int[] ycoord = {yCenter - 128, yCenter + 128, yCenter + 128, yCenter - 128};
+        int [] xcoord = {xCenter - 128, xCenter, xCenter + 128};
+        int [] ycoord = {yCenter, yCenter - 128, yCenter};
 
         g.setColor(Color.RED);
         drawAndSplit(g, xcoord, ycoord, 1);
 
     }
 
-    public int[] midpoints(int[] x) {
-        int[] m = new int[4];
+    public int [] midpoints(int [] x)
+    {
+        int [] m = new int [4];
 
-        m[0] = (x[0] + x[1]) / 2;
-        m[1] = (x[1] + x[2]) / 2;
-        m[2] = (x[2] + x[3]) / 2;
-        m[3] = (x[3] + x[0]) / 2;
+        m[0] = (x[0] + x[1])/2;
+        m[1] = (x[1] + x[2])/2;
+        m[2] = (x[2] + x[3])/2;
+        m[3] = (x[3] + x[0])/2;
 
         return m;
     }
 
-    public void drawAndSplit(Graphics g, int[] x, int[] y, int times) {
+    public void drawAndSplit(Graphics g, int [] x, int [] y, int times)
+    {
         if (times != 0) {
-            g.drawPolygon(x, y, 4);
+            g.drawPolygon(x, y, 3);
             drawAndSplit(g, midpoints(x), midpoints(y), times - 1);
         }
     }
